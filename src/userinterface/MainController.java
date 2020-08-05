74c46e25235bdc4960ffdf23a7cf095a0b509af2
Nicolas Penagos Montoya <nicolas.penagosm98@gmail.com>
2020-08-05 17:51:15 -0500

    Validations to add a vehicle have been coded

--- src/userinterface/MainController.java
+++ src/userinterface/MainController.java
@@ -13,6 +13,8 @@ import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.List;
 
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
 import javafx.collections.FXCollections;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
@@ -33,11 +35,34 @@ import threads.TimeThread;
 
 public class MainController {
 
+	private static final int LIMIT = 1;
+
 	@FXML
 	private ImageView title;
 
 	@FXML
 	private Pane sepPane;
+
+	@FXML
+	private Label av1;
+
+	@FXML
+	private Label av2;
+
+	@FXML
+	private Label av3;
+
+	@FXML
+	private Label avR;
+
+	@FXML
+	private Label avH;
+
+	@FXML
+	private Label avP;
+
+	@FXML
+	private Label avT;
 
 	@FXML
 	private Label lb1;
@@ -65,13 +90,12 @@ public class MainController {
 
 	@FXML
 	private Button saveButton;
-	
 
-    @FXML
-    private Button cancelButton;
+	@FXML
+	private Button cancelButton;
 
-    @FXML
-    private Button confirmButton;
+	@FXML
+	private Button confirmButton;
 
 	@FXML
 	private Label errorLb;
@@ -135,48 +159,57 @@ public class MainController {
 
 	@FXML
 	private MenuItem addUserMenuBar;
-	
-    @FXML
-    private Label ivLb4;
 
-    @FXML
-    private TextField p1;
+	@FXML
+	private Label ivLb4;
 
-    @FXML
-    private Label ivLb6;
+	@FXML
+	private TextField p1;
 
-    @FXML
-    private TextField p2;
+	@FXML
+	private Label ivLb6;
 
-    @FXML
-    private TextField p3;
+	@FXML
+	private TextField p2;
 
-    @FXML
-    private TextField p4;
+	@FXML
+	private TextField p3;
 
-    @FXML
-    private TextField p5;
+	@FXML
+	private TextField p4;
 
-    @FXML
-    private TextField p6;
+	@FXML
+	private TextField p5;
 
-    @FXML
-    private ComboBox<String> type;
+	@FXML
+	private TextField p6;
 
-    @FXML
-    private Label ivLb5;
+	@FXML
+	private ComboBox<String> type;
 
-    @FXML
-    private Label ivLb1;
+	@FXML
+	private Label ivLb5;
 
-    @FXML
-    private Label ivLb2;
+	@FXML
+	private Label ivLb1;
 
-    @FXML
-    private Label ivLb3;
+	@FXML
+	private Label av0;
+
+	@FXML
+	private Label ivLb3;
 
 	@FXML
 	private MenuBar menubar;
+
+	@FXML
+	private Label addVehicleErrorLabel;
+
+	@FXML
+	private Button cancelButton1;
+
+	@FXML
+	private Button confirmButton1;
 
 	private Logic logic;
 	private AlertBoxAddUser addUserWindow;
@@ -198,16 +231,29 @@ public class MainController {
 		enterVehicleButton.setGraphic(new ImageView(new Image("/images/enterV.png")));
 		removeVehicleButton.setGraphic(new ImageView(new Image("/images/exitV-01.png")));
 
+		addVehicleErrorLabel.setVisible(false);
+		cancelButton1.setVisible(false);
+		confirmButton1.setVisible(false);
 		sepPane2.setVisible(false);
 		sepPane3.setVisible(false);
 		sepPane4.setVisible(false);
 
+		av0.setVisible(false);
+		av1.setVisible(false);
+		av2.setVisible(false);
+		av3.setVisible(false);
+
+		avH.setVisible(false);
+		avP.setVisible(false);
+		avR.setVisible(false);
+		avT.setVisible(false);
+
 		enterVehicleButton.setVisible(false);
 		removeVehicleButton.setVisible(false);
-		
-		//cancelButton.setGraphic(new ImageView(new Image("/images/cancel-01.png")));
+
+		// cancelButton.setGraphic(new ImageView(new Image("/images/cancel-01.png")));
 		cancelButton.setVisible(false);
-		//confirmButton.setGraphic(new ImageView(new Image("/images/confirm-01.png")));
+		// confirmButton.setGraphic(new ImageView(new Image("/images/confirm-01.png")));
 		confirmButton.setVisible(false);
 
 		addUserMenuBar.setDisable(true);
@@ -220,23 +266,136 @@ public class MainController {
 		sepPane111.setVisible(false);
 		sepPane1111.setVisible(false);
 		sepPane111111.setVisible(false);
-		
+
 		p1.setVisible(false);
 		p2.setVisible(false);
 		p3.setVisible(false);
 		p4.setVisible(false);
 		p5.setVisible(false);
 		p6.setVisible(false);
-		
+
+		p1.lengthProperty().addListener(new ChangeListener<Number>() {
+
+			@Override
+			public void changed(ObservableValue<? extends Number> observable, Number oldValue, Number newValue) {
+				if (newValue.intValue() > oldValue.intValue()) {
+					// Check if the new character is greater than LIMIT
+					if (p1.getText().length() >= LIMIT) {
+
+						// if it's 11th character then just setText to previous
+						// one
+						p1.setText(p1.getText().substring(0, LIMIT).toUpperCase());
+						if (!Character.isLetter(p1.getText().charAt(0)))
+							p1.setText("");
+
+					}
+				}
+			}
+		});
+
+		p2.lengthProperty().addListener(new ChangeListener<Number>() {
+
+			@Override
+			public void changed(ObservableValue<? extends Number> observable, Number oldValue, Number newValue) {
+				if (newValue.intValue() > oldValue.intValue()) {
+					// Check if the new character is greater than LIMIT
+					if (p2.getText().length() >= LIMIT) {
+
+						// if it's 11th character then just setText to previous
+						// one
+						p2.setText(p2.getText().substring(0, LIMIT).toUpperCase());
+						if (!Character.isLetter(p2.getText().charAt(0)))
+							p2.setText("");
+
+					}
+				}
+			}
+		});
+
+		p3.lengthProperty().addListener(new ChangeListener<Number>() {
+
+			@Override
+			public void changed(ObservableValue<? extends Number> observable, Number oldValue, Number newValue) {
+				if (newValue.intValue() > oldValue.intValue()) {
+					// Check if the new character is greater than LIMIT
+					if (p3.getText().length() >= LIMIT) {
+
+						// if it's 11th character then just setText to previous
+						// one
+						p3.setText(p3.getText().substring(0, LIMIT).toUpperCase());
+						if (!Character.isLetter(p3.getText().charAt(0)))
+							p3.setText("");
+
+					}
+				}
+			}
+		});
+
+		p4.lengthProperty().addListener(new ChangeListener<Number>() {
+
+			@Override
+			public void changed(ObservableValue<? extends Number> observable, Number oldValue, Number newValue) {
+				if (newValue.intValue() > oldValue.intValue()) {
+					// Check if the new character is greater than LIMIT
+					if (p4.getText().length() >= LIMIT) {
+
+						// if it's 11th character then just setText to previous
+						// one
+						p4.setText(p4.getText().substring(0, LIMIT).toUpperCase());
+						if (!Character.isDigit(p4.getText().charAt(0)))
+							p4.setText("");
+
+					}
+				}
+			}
+		});
+
+		p5.lengthProperty().addListener(new ChangeListener<Number>() {
+
+			@Override
+			public void changed(ObservableValue<? extends Number> observable, Number oldValue, Number newValue) {
+				if (newValue.intValue() > oldValue.intValue()) {
+					// Check if the new character is greater than LIMIT
+					if (p5.getText().length() >= LIMIT) {
+
+						// if it's 11th character then just setText to previous
+						// one
+						p5.setText(p5.getText().substring(0, LIMIT).toUpperCase());
+						if (!Character.isDigit(p5.getText().charAt(0)))
+							p5.setText("");
+
+					}
+				}
+			}
+		});
+
+		p6.lengthProperty().addListener(new ChangeListener<Number>() {
+
+			@Override
+			public void changed(ObservableValue<? extends Number> observable, Number oldValue, Number newValue) {
+				if (newValue.intValue() > oldValue.intValue()) {
+					// Check if the new character is greater than LIMIT
+					if (p6.getText().length() >= LIMIT) {
+
+						// if it's 11th character then just setText to previous
+						// one
+						p6.setText(p6.getText().substring(0, LIMIT).toUpperCase());
+						if (!Character.isDigit(p6.getText().charAt(0)))
+							p6.setText("");
+
+					}
+				}
+			}
+		});
+
 		ivLb1.setVisible(false);
-		ivLb2.setVisible(false);
+		av0.setVisible(false);
 		ivLb3.setVisible(false);
 		ivLb4.setVisible(false);
 		ivLb5.setVisible(false);
 		ivLb6.setVisible(false);
-	
+
 		type.setVisible(false);
-	
 
 		GUIUpdateControllThread guiThread = new GUIUpdateControllThread(this);
 		guiThread.setDaemon(true);
@@ -480,29 +639,41 @@ public class MainController {
 
 	@FXML
 	void registerVehicle(ActionEvent event) {
-		
+
+		/*
 		p1.setVisible(true);
 		p2.setVisible(true);
 		p3.setVisible(true);
 		p4.setVisible(true);
 		p5.setVisible(true);
 		p6.setVisible(true);
-		
+
+		p1.setText("");
+		p2.setText("");
+		p3.setText("");
+		p4.setText("");
+		p5.setText("");
+		p6.setText("");
+
+		type.setValue("");
+
 		ivLb1.setVisible(true);
-		ivLb2.setVisible(true);
 		ivLb3.setVisible(true);
 		ivLb4.setVisible(true);
 		ivLb5.setVisible(true);
 		ivLb6.setVisible(true);
-		
+
 		type.setVisible(true);
-		
+
 		enterVehicleButton.setVisible(false);
 		removeVehicleButton.setVisible(false);
-		
+
 		cancelButton.setVisible(true);
 		confirmButton.setVisible(true);
-		//logic.AddVehicle(Calendar.getInstance());
+		// logic.AddVehicle(Calendar.getInstance());
+		 * */
+		
+		 setVisibleRegisterVehicle(true);
 	}
 
 	@FXML
@@ -526,57 +697,193 @@ public class MainController {
 
 		enterVehicleButton.setVisible(false);
 		removeVehicleButton.setVisible(false);
-		
+
 		p1.setVisible(false);
 		p2.setVisible(false);
 		p3.setVisible(false);
 		p4.setVisible(false);
 		p5.setVisible(false);
 		p6.setVisible(false);
-		
+
 		ivLb1.setVisible(false);
-		ivLb2.setVisible(false);
+		av0.setVisible(false);
 		ivLb3.setVisible(false);
 		ivLb4.setVisible(false);
 		ivLb5.setVisible(false);
 		ivLb6.setVisible(false);
-		
+
 		type.setVisible(false);
-		
+
 		sepPane4.setVisible(false);
 		sepPane2.setVisible(false);
 		sepPane3.setVisible(false);
-		
+
 		enterVehicleButton.setVisible(false);
 		removeVehicleButton.setVisible(false);
-		
+
 		cancelButton.setVisible(false);
 		confirmButton.setVisible(false);
 
 	}
-	
+
 	@FXML
-    void cancel(ActionEvent event) {
+	void cancel(ActionEvent event) {
 		p1.setVisible(false);
 		p2.setVisible(false);
 		p3.setVisible(false);
 		p4.setVisible(false);
 		p5.setVisible(false);
 		p6.setVisible(false);
-		
+
+		av0.setVisible(false);
+		av1.setVisible(false);
+		av2.setVisible(false);
+		av3.setVisible(false);
+
+		avH.setVisible(false);
+		avP.setVisible(false);
+		avR.setVisible(false);
+		avT.setVisible(false);
+
 		ivLb1.setVisible(false);
-		ivLb2.setVisible(false);
+		av0.setVisible(false);
 		ivLb3.setVisible(false);
 		ivLb4.setVisible(false);
 		ivLb5.setVisible(false);
 		ivLb6.setVisible(false);
-		
+
 		type.setVisible(false);
-		
+		addVehicleErrorLabel.setVisible(false);
+
 		enterVehicleButton.setVisible(true);
 		removeVehicleButton.setVisible(true);
-		
+
 		cancelButton.setVisible(false);
 		confirmButton.setVisible(false);
-    }
+	}
+
+	@FXML
+	void addVehicle(ActionEvent event) {
+
+		if (!p1.getText().equals("") && !p2.getText().equals("") && !p3.getText().equals("") && !p4.getText().equals("")
+				&& !p5.getText().equals("") && !p6.getText().equals("") && !type.getValue().equals("")) {
+
+			addVehicleErrorLabel.setVisible(false);
+			p1.setVisible(false);
+			p2.setVisible(false);
+			p3.setVisible(false);
+			p4.setVisible(false);
+			p5.setVisible(false);
+			p6.setVisible(false);
+
+			ivLb1.setVisible(false);
+			av0.setVisible(false);
+			ivLb3.setVisible(false);
+			ivLb4.setVisible(false);
+			ivLb5.setVisible(false);
+			ivLb6.setVisible(false);
+
+			type.setVisible(false);
+
+			String plate = p1.getText() + p2.getText() + p3.getText() + "-" + p4.getText() + p5.getText()
+					+ p6.getText();
+			String responsable = logic.getCurrentUser().getName();
+			String sType = type.getValue();
+			DateFormat dateFormat = new SimpleDateFormat("yyyy/MM/dd HH:mm:ss");
+			Calendar cal = Calendar.getInstance();
+			String sDate = dateFormat.format(cal.getTime());
+
+			ivLb1.setText("Por favor verifique que los datos ingresados son correctos:");
+			ivLb1.setVisible(true);
+
+			setConfirmationVehicleVisible(true);;
+		
+			avR.setText(responsable);
+			avH.setText(sDate);
+			avT.setText(sType);
+			avP.setText(plate);
+
+		
+
+		} else {
+
+			addVehicleErrorLabel.setVisible(true);
+
+		}
+	}
+
+	@FXML
+	void okAdd(ActionEvent event) {
+
+	}
+
+	@FXML
+	void cancel1(ActionEvent event) {
+		setVisibleRegisterVehicle(true);
+		setConfirmationVehicleVisible(false);
+	}
+	
+	public void setVisibleRegisterVehicle(boolean val) {
+		
+		p1.setVisible(val);
+		p2.setVisible(val);
+		p3.setVisible(val);
+		p4.setVisible(val);
+		p5.setVisible(val);
+		p6.setVisible(val);
+
+		p1.setText("");
+		p2.setText("");
+		p3.setText("");
+		p4.setText("");
+		p5.setText("");
+		p6.setText("");
+
+		type.setValue("");
+
+		ivLb1.setVisible(val);
+		ivLb3.setVisible(val);
+		ivLb4.setVisible(val);
+		ivLb5.setVisible(val);
+		ivLb6.setVisible(val);
+
+		type.setVisible(val);
+
+		enterVehicleButton.setVisible(val);
+		removeVehicleButton.setVisible(val);
+
+		cancelButton.setVisible(val);
+		confirmButton.setVisible(val);
+		
+		enterVehicleButton.setVisible(!val);
+		removeVehicleButton.setVisible(!val);
+		
+		ivLb1.setText("Por favor registre los siguientes datos para ingresar un vehículo:");
+		ivLb1.setVisible(val);
+
+		
+	}
+	
+	public void setConfirmationVehicleVisible(boolean val) {
+
+		ivLb1.setText("Por favor verifique que los datos ingresados son correctos:");
+		ivLb1.setVisible(true);
+
+		av0.setVisible(val);
+		av1.setVisible(val);
+		av2.setVisible(val);
+		av3.setVisible(val);
+
+		avH.setVisible(val);
+		avP.setVisible(val);
+		avR.setVisible(val);
+		avT.setVisible(val);
+		
+		cancelButton1.setVisible(val);
+		cancelButton.setVisible(!val);
+		
+		confirmButton.setVisible(!val);
+		confirmButton1.setVisible(val);
+	}
+
 }
